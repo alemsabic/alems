6e8319a3fab406b0e164fbdb376c19ff555f1629
Alem <alemsabic@gmail.com>
2021-01-29 09:48:04 +0100

    Initial commit

---
+++ docs/assets/codedoc-bundle.entry.js
@@ -0,0 +1,42 @@
+import { getRenderer } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/transport/renderer.js';
+import { initJssCs } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/transport/setup-jss.js';initJssCs();
+import { installTheme } from '/Users/admin/Desktop/alems.org/.codedoc/content/theme.ts';installTheme();
+import { codeSelection } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/selection.js';codeSelection();
+import { sameLineLengthInCodes } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/same-line-length.js';sameLineLengthInCodes();
+import { initHintBox } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-hint/index.js';initHintBox();
+import { initCodeLineRef } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-ref/index.js';initCodeLineRef();
+import { initSmartCopy } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/smart-copy.js';initSmartCopy();
+import { copyHeadings } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/components/heading/copy-headings.js';copyHeadings();
+import { contentNavHighlight } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/contentnav/highlight.js';contentNavHighlight();
+import { loadDeferredIFrames } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/transport/deferred-iframe.js';loadDeferredIFrames();
+import { smoothLoading } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/transport/smooth-loading.js';smoothLoading();
+import { tocHighlight } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toc-highlight.js';tocHighlight();
+import { postNavSearch } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
+import { copyLineLinks } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-links/copy-line-link.js';copyLineLinks();
+import { gatherFootnotes } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/components/footnote/gather-footnotes.js';gatherFootnotes();
+import { ToCPrevNext } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/prevnext/index.js';
+import { ToCToggle } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toggle/index.js';
+import { DarkModeSwitch } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
+import { ConfigTransport } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
+import { TabSelector } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/components/tabs/selector.js';
+import { CollapseControl } from '/Users/admin/Desktop/alems.org/.codedoc/node_modules/@codedoc/core/dist/es6/components/collapse/collapse-control.js';
+
+const components = {
+  'FRi75tQM2IJCEJqClvYBFg==': ToCPrevNext,
+  'tu2NYv2a/U2oboTHbtp85g==': ToCToggle,
+  'VTiQkZprqSp0WRbj890mVA==': DarkModeSwitch,
+  '5p/RgT2Mf74iZzUVZwBhbg==': ConfigTransport,
+  'N1bILrZiVxUANXITyt1Uag==': TabSelector,
+  'i2pLJ2QrIFbKrwHobzUX7g==': CollapseControl
+};
+
+const renderer = getRenderer();
+const ogtransport = window.__sdh_transport;
+window.__sdh_transport = function(id, hash, props) {
+  if (hash in components) {
+    const target = document.getElementById(id);
+    renderer.render(renderer.create(components[hash], props)).after(target);
+    target.remove();
+  }
+  else if (ogtransport) ogtransport(id, hash, props);
+}
