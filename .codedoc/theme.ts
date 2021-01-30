3c1578869eb2b638edf23a198f30b27da69a0ffb
Alem <alemsabic@gmail.com>
2021-01-30 13:09:07 +0100

    Update theme.ts

--- .codedoc/theme.ts
+++ .codedoc/theme.ts
@@ -3,9 +3,19 @@ import { createTheme } from '@codedoc/core/transport';
 
 export const theme = /*#__PURE__*/createTheme({
   light: {
-    primary: '#1eb2a6'
+    background: '#eeeeee',           // --> CSS color string, background of the page
+    text: '#6653ff',            // --> CSS color string, color of text
+    primary: '#6653ff',             // --> CSS color string, primary color (for buttons, links, etc)
+    primaryContrast: '#eeeeee',      // --> CSS color string, text color on primary background (e.g. buttons)
+    border: '#b1abe0',               // --> CSS color string, border colors
+    code: '#6653ff',                  // --> CSS color string, in-text code color
   },
   dark: {
-    primary: '#1eb2a6'
+    background: '#000000',           // --> CSS color string, background of the page
+    text: '#69f0ae',              // --> CSS color string, color of text
+    primary: '#69f0ae',            // --> CSS color string, primary color (for buttons, links, etc)
+    primaryContrast: '#000000',  // --> CSS color string, text color on primary background (e.g. buttons)
+    border: '#315f48',      // --> CSS color string, border colors
+    code: '#69f0ae',                  // --> CSS color string, in-text code color
   }
 });
