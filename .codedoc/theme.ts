7887993aff28f5d2abb91277693b06b656233d4e
Alem <alemsabic@gmail.com>
2021-01-30 13:28:11 +0100

    Update theme.ts

--- .codedoc/theme.ts
+++ .codedoc/theme.ts
@@ -3,10 +3,10 @@ import { createTheme } from '@codedoc/core/transport';
 
 export const theme = /*#__PURE__*/createTheme({
   light: {
-    background: '#eeeeee',           // --> CSS color string, background of the page
+    background: '#dddddd',           // --> CSS color string, background of the page
     text: '#6653ff',            // --> CSS color string, color of text
     primary: '#6653ff',             // --> CSS color string, primary color (for buttons, links, etc)
-    primaryContrast: '#eeeeee',      // --> CSS color string, text color on primary background (e.g. buttons)
+    primaryContrast: '#dddddd',      // --> CSS color string, text color on primary background (e.g. buttons)
     border: '#b1abe0',               // --> CSS color string, border colors
     code: '#6653ff',                  // --> CSS color string, in-text code color
   },
