a830bdcb4601006a3a9e84a905b3ffa7738d5373
Alem <alemsabic@gmail.com>
2021-02-16 13:18:43 +0100

    Update config.tsx

--- .codedoc/config.tsx
+++ .codedoc/config.tsx
@@ -58,10 +58,8 @@ export const config = /*#__PURE__*/configuration({
 
 
     scripts: [
-      <meta name="msapplication-TileColor" content="#2b5797"/>,
       <link rel="manifest" href="/site.webmanifest"/>,
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>,
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>,
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>,
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>,
     ],                       
