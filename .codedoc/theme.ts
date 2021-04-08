fd4f492b8ba2349d8a25295b4e0fd92e4f18f2dc
Alem <alemsabic@gmail.com>
2021-04-08 08:40:07 +0200

    Shadow, baby!

--- .codedoc/theme.ts
+++ .codedoc/theme.ts
@@ -11,16 +11,16 @@ export const theme = /*#__PURE__*/createTheme({
     code: '#555',                  // --> CSS color string, in-text code color
   },
   dark: {
-    background: '#333',           // --> CSS color string, background of the page
+    background: '#555',           // --> CSS color string, background of the page
     text: '#eee',              // --> CSS color string, color of text
     primary: '#b4a142',            // --> CSS color string, primary color (for buttons, links, etc)
-    primaryContrast: '#333',  // --> CSS color string, text color on primary background (e.g. buttons)
+    primaryContrast: '#555',  // --> CSS color string, text color on primary background (e.g. buttons)
     border: 'rgba(255,255,255,0.2)',      // --> CSS color string, border colors
     code: '#eee',                  // --> CSS color string, in-text code color
   },
   quote: {
     dark: {
-      background: '#333',
+      background: '#555',
       text: '#eee', 
       border: 'rgba(255,255,255,0.2)',
     },
@@ -36,7 +36,7 @@ export const theme = /*#__PURE__*/createTheme({
       border: 'rgba(0,0,0,0.2)',                 // --> CSS color string, border of ToC
     },
     dark: {
-      background: 'rgba(52,51,51,0.6)',             // --> CSS color string, background of ToC
+      background: 'rgba(85,85,85,0.6)',             // --> CSS color string, background of ToC
       border: 'rgba(255,255,255,0.2)',                // --> CSS color string, border of ToC
     },             
   },
